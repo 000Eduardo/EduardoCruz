e17e3861cd1f72ecf46c22bd94f001881f2d735f
Eduardo <miguel.cruz@uniat.edu.mx>
2023-09-21 20:15:38 -0700

    fist commit

---
+++ Assets/Scripts/cubo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubo : MonoBehaviour
+{
+
+    private void Update()
+    {
+
+        this.transform.Rotate(0f, 0.2f, 0f);
+       
+    }
+
+}
